d2c80905897d6bfa282a5da5fceb0de87e5221e8
William <williambecker321@gmail.com>
2022-02-02 13:21:59 +0100

    Added randomased amount of content

--- body.js
+++ body.js
@@ -5,19 +5,28 @@ const fun = () => {
     document.title = 'William was here'
 }
 
-const rowClass = ['row', 'align-items-start', 'justify-content-center'];
+const randomText = () => {
+    let string = '';
+    
+    const iterate = Math.floor(Math.random() * 30) + 10;
+
+    for (let index = 0; index < iterate; index++) {
+        string += 'Random text. '
+    }
+    return string;
+}
 
 // First
 const body = document.getElementById('body');
 body.classList.add('container');
 
 const firstRow = document.createElement('div');
-firstRow.classList.add(rowClass);
+firstRow.classList.add('row', 'justify-content-center');
 
 const container1 = document.createElement('div');
 container1.classList.add('col-8')
 
-const title = document.createElement('p');
+const title = document.createElement('h1');
 title.classList.add('text-center', 'h1', 'mx-auto');
 title.textContent = 'Body is alive';
 
@@ -25,6 +34,23 @@ body.append(firstRow);
 firstRow.append(container1);
 container1.append(title);
 
+// More rows
+const moreRows = document.createElement('div');
+moreRows.classList.add('row', 'justify-content-around');
+moreRows.classList.remove('justify-content-center');
+
+for (let index = 0; index < 4; index++) {
+    const containers = document.createElement('div');
+    containers.classList.add('col-3', 'border', 'border-danger', 'border-5');
+
+    const paragraf = document.createElement('p');
+    paragraf.textContent = randomText();
+    containers.append(paragraf);
+
+    moreRows.append(containers);
+}
+
+body.append(moreRows);
 
 
 setTimeout(fun, 15000);
